df13c33b2af692e5ea6d709221258624bc4b949c
zacharyooper67 <zacharycooper@seattleacademy.org>
2025-09-22 19:17:22 +0000

    project 2

---
+++ Projects/granny.py
@@ -0,0 +1,8 @@
+while True:
+    word = input ("What do you think grandma likes?")
+    if "t" in word: 
+        print (f"grandma doesn't like {word} ! ")
+    else:
+        print (f"grandma likes {word}")
+    print("")
+    
---
+++ Projects/personality_quiz.py
@@ -0,0 +1,65 @@
+vanilla_points=0
+chocolate_points=0
+
+
+answer=input(" are you a), morning person or b a night person")
+if answer=="a":
+    vanilla_points+=1
+elif answer=="b":
+    chocolate_points+=1
+
+
+answer=input(" would you rather a go outside or b stay inside.")
+if answer=="a":
+    vanilla_points+=1
+elif answer=="b":
+    chocolate_points+=1
+
+
+answer=input("are you a an cat person or b a dog person")
+if answer=="a":
+    vanilla_points+=1
+elif answer=="b":
+    chocolate_points+=1
+
+
+answer=input("do you a like hot weather or b like cold weather")
+if answer=="a":
+    vanilla_points+=1
+elif answer=="b":
+    chocolate_points+=1
+
+
+
+answer=input("do you like a or A sprite or b coke")
+if answer=="a":
+    vanilla_points+=1
+elif answer=="b":
+    chocolate_points+=1
+print(f"vanilla points = {vanilla_points}")
+print(f"chocolate points = {chocolate_points}")
+if chocolate_points> vanilla_points:
+    print("you are a chocolate person.")
+elif vanilla_points> chocolate_points:
+    print("you are a movie person")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
