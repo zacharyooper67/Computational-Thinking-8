033702f1325cb798c7dac20924214995514c184e
zacharyooper67 <zacharycooper@seattleacademy.org>
2025-11-14 19:29:35 +0000

    hi

---
+++ Projects/p5.py
@@ -0,0 +1,25 @@
+import random
+
+print("     ________________________________     ")
+print("     |                              |     ")
+print("     |                              |     ")
+print("     |       Penalty Shootout       |     ")
+print("     |                              |     ")
+print("_____|______________________________|_____")
+print("")
+print("")
+
+score = 0
+
+for i in range(0,5):
+        options=["TL","BL","M","TR","BR"]
+        computerOption = random.choice(options)
+
+        userOption = input("where do you want to shoot?") ( TL, BL, M, TR, BR)
+
+        if computerOption == userOption: 
+            print("blocked")
+        else:
+            print("GOAL!!")
+score += 1
+print("your score:" + str (score))
