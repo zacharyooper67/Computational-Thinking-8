74e2bc1957823fc5a6467ebb02d8d58fa5fd3cc5
zacharyooper67 <zacharycooper@seattleacademy.org>
2025-11-20 19:31:35 +0000

    hi

--- Projects/p5.py
+++ Projects/p5.py
@@ -15,7 +15,7 @@ for i in range(0,5):
         options=["TL","BL","M","TR","BR"]
         computerOption = random.choice(options)
 
-        userOption = input("where do you want to shoot?") ( TL, BL, M, TR, BR)
+        userOption = input("where do you want to shoot? TL,TR,M,BL,BR") 
 
         if computerOption == userOption: 
             print("blocked")
